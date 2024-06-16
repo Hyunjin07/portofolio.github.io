b75504c8901325bd54ae5b50ec41962707a50fb8
ca970 <ca5522397@gmail.com>
2024-06-16 01:42:10 -0400

    updated Navbar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,14 @@
-import logo from "./logo.svg";
+import React from "react";
+import Navbar from "./components/Navbar";
+
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import "../App.css";
 
-function Navbar() {
+const Navbar = () => {
+  const handleNavClick = (event) => {
+    const target = event.target;
+    const link = target.getAttribute("data-link");
+    if (link) {
+      const section = document.querySelector(link);
+      if (section) {
+        section.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+  };
+
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="navbar">
+      <ul className="navbar__menu" onClick={handleNavClick}>
+        <li className="navbar__menu__item active" data-link="#home">
+          Home
+        </li>
+        <li className="navbar__menu__item" data-link="#about">
+          About
+        </li>
+        <li className="navbar__menu__item" data-link="#skills">
+          Skills
+        </li>
+        <li className="navbar__menu__item" data-link="#work">
+          My work
+        </li>
+        <li className="navbar__menu__item" data-link="#service">
+          Service
+        </li>
+        <li className="navbar__menu__item" data-link="#contact">
+          Contact
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
